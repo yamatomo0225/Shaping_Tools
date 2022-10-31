9b13be77c6a9543b2709e6bd17563467ee6758a4
yamatomo0225 <yamatomo0225@gmail.com>
2022-11-01 01:25:41 +0900

    RAW_LOG作成

--- JS/SEIKEI.js
+++ JS/SEIKEI.js
@@ -1,20 +1,17 @@
 'use strict';
 
 function SEIKEI_FORTIGATE(){
-    var FORM_FORTIGATE = document.getElementById('FORM_FORTIGATE').value;
-    var CONTENTS_OF_LOG_FORTIGATE = LOG_FORTIGATE.map(/(?<=\=\").+?(?=\" )|(?<=\=)[0-9].*?[0-9]?(?= )/g);
+    const FORM_FORTIGATE = [];
+    
+    var RAW_LOG = document.getElementById('FORM_FORTIGATE').value;
+    RAW_LOG.innerHTML = RAW_LOG
+    var CONTENTS_OF_LOG_FORTIGATE = FORM_FORTIGATE.map(/(?<=\=\").+?(?=\" )|(?<=\=)[0-9].*?[0-9]?(?= )/g);
 
-    FORM_FORTIGATE = FORM_FORTIGATE.replace(//g,`\"${/(?<=\=)[0-9].*?[0-9]?(?= )/g}\"`);
-    console.log(CONTENTS_OF_LOG_FORTIGATE);
+    FORM_FORTIGATE = FORM_FORTIGATE.replace(//g,`\"${/(?<=\=)[0-9].*?[0-9]?(?= )/,g}\"`);
     
     //FORM_FORTIGATE.map( );
     //""で囲まれた内容を抽出するために""を付与している。
-    //
-    
     //var category_of_LOG_FORTIGATE = LOG_FORTIGATE.map();                                                 
-    //
-    
     //document.getElementById('after').innerHTML = LOG_FORTIGATE.join('<br>') 
-
     //console.log(contents_of_LOG_FORTIGATE)
-}
+    }
