927c04a2031cf321c34821c7a6a000aa41fb21b8
yamatomo0225 <yamatomo0225@gmail.com>
2022-11-15 22:00:14 +0900

    LOGの抽出結果を修正

--- JS/SEIKEI.js
+++ JS/SEIKEI.js
@@ -1,23 +1,23 @@
 'use strict';
 function SEIKEI_FORTIGATE(){
 //連想配列に格納する
-    var RAW_LOG = document.getElementById('FORM_FORTIGATE').value;
-    RAW_LOG = RAW_LOG.replace(/\"/g,'');
-    var RAW_LOG_array = RAW_LOG.split(/(?<!:) /g);
-    RAW_LOG_array[0] = RAW_LOG_array[0].replace(/-/g,'/');
+    var RAW_LOG = [...document.getElementById('FORM_FORTIGATE').value.matchAll(/\b\w+=(".*?"|[^" ]+)/g)].map(m => m[0]);
+    for(var i=0; i<RAW_LOG.length; i++){
+        RAW_LOG[i] = RAW_LOG[i].replace(/"/g,'');
+      }
 
-
+    console.log(RAW_LOG)
     var LOG = {};
-    for(var i=0;i<RAW_LOG_array.length;i++){
-        LOG[RAW_LOG_array[i].match(/.*?(?=\=)/s)]= RAW_LOG_array[i].match(/(?<=\=).*/s);
+    for(var i=0;i<RAW_LOG.length;i++){
+        LOG[RAW_LOG[i].match(/.*(?==)/s)]= RAW_LOG[i].match(/(?<==).*/s);
     }
-    console.log(RAW_LOG_array)
+    console.log(LOG)
 
 //ログ確認する際によく見る内容だけ抽出表示　見づらいので後で調整予定？
     document.getElementById("extract").innerHTML = `日時:${LOG.date} ${LOG.time}<br>severity:${LOG.severity}<br>attack:${LOG.attack}<br>action:${LOG.action}<br>通信経路${LOG.srcip}(Port:${LOG.srcport}) → ${LOG.dstip}(Port:${LOG.dstport}) `;
        
 //ログの内容を全て表示
     var ALL_LOG = '';
-    RAW_LOG_array.forEach(value=> ALL_LOG +=`<li>${value}</li>`)
+    RAW_LOG.forEach(value=> ALL_LOG +=`<li>${value}</li>`)
     document.getElementById("ALL_LOG").innerHTML = ALL_LOG;
 }
---
+++ JS/standard.js
@@ -0,0 +1,19 @@
+timerID = setInterval('clock()',500); //0.5秒毎にclock()を実行
+
+function clock() {
+	document.getElementById("view_clock").innerHTML = getNow();
+}
+
+function getNow() {
+	var now = new Date();
+	var year = now.getFullYear();
+	var mon = (now.getMonth()+1).toString().padStart(2, '0'); 
+	var day = now.getDate().toString().padStart(2, '0');
+	var hour = now.getHours().toString().padStart(2, '0');
+	var min = now.getMinutes().toString().padStart(2, '0');
+	var sec = now.getSeconds().toString().padStart(2, '0');
+
+	//出力用
+	var s = year + "/" + mon + "/" + day + " " + hour + ":" + min + ":" + sec; 
+	return s;
+}
